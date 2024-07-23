b045089d643dede1902309f14bd4fc1be4149ce5
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-07-23 19:03:42 +0700

    add ws client

---
+++ context/WsProvider.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { ReactNode } from 'react';
+import { useAuth } from './AuthProvider';
+import { ApolloProvider } from '@apollo/client';
+import createWsClient from '@/services/graphql';
+
+interface WsProviderProps {
+  children: ReactNode;
+}
+
+const WsProvider = ({ children }: WsProviderProps) => {
+  const { user } = useAuth();
+  return (
+    <ApolloProvider client={createWsClient(user?.token)}>
+      {children}
+    </ApolloProvider>
+  );
+};
+
+export default WsProvider;
+
+const styles = StyleSheet.create({});
