2821ddade2004918f64d29d8a0e7ff56b69848de
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-07-28 13:49:19 +0700

    FE-28: add members screen

--- app/(tabs)/servers/index.tsx
+++ app/(tabs)/servers/index.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { fonts } from '@/constants/theme';
+import { Link } from 'expo-router';
 
 export default function Servers() {
   return (
@@ -11,6 +12,7 @@ export default function Servers() {
       >
         Tab Servers Hello
       </Text>
+      <Link href="/servers/members">Members</Link>
     </View>
   );
 }
