7a5a3f9e1fd3d29a0260c360ede8e2057e717a03
Truong Chi Nhan <nhanbin03@gmail.com>
2024-07-28 10:28:52 +0700

    FE-25: Update WsProvider to use getIdToken from auth service

--- context/WsProvider.tsx
+++ context/WsProvider.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { ReactNode, useEffect, useState } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import createWsClient from '@/services/graphql';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getIdToken } from '@/services/auth';
 
 interface WsProviderProps {
   children: ReactNode;
@@ -13,7 +13,7 @@ const WsProvider = ({ children }: WsProviderProps) => {
 
   useEffect(() => {
     (async () => {
-      const token = await AsyncStorage.getItem('idToken');
+      const token = await getIdToken();
       setToken(token);
     })();
   }, []);
