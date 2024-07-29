ad2363b7c96e04ffec8570d0d5ab564dfa21857c
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-07-29 22:21:32 +0700

    FE-28: add touchable wrap the modalView

--- components/modal/FilterModal.tsx
+++ components/modal/FilterModal.tsx
@@ -33,58 +33,60 @@ const FilterModal = (props: FilterModalProps) => {
       }}
     >
       <TouchableWithoutFeedback onPress={props.onClose}>
-        <View style={styles.modelContainer}>
-          <View style={styles.modalView}>
-            <MyText style={styles.title}>Filter</MyText>
-            <View style={styles.body}>
-              <MyText style={[TextStyles.bodyM, { color: colors.gray02 }]}>
-                You can select multiple roles
-              </MyText>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                marginTop: 8,
-                marginBottom: 16
-              }}
-            >
-              <ButtonListCheckbox
-                heading="Select Roles"
-                items={Array.from({ length: 7 }, (_, index) => ({
-                  value: `role-${index}`,
-                  label: `Moderator`
-                }))}
-                scrollable
-                values={selectedRoles}
-                onAdd={(value: string) =>
-                  setSelectedRoles([...selectedRoles, value])
-                }
-                onRemove={(value: string) =>
-                  setSelectedRoles(
-                    selectedRoles.filter((role) => role !== value)
-                  )
-                }
+        <View style={styles.modalContainer}>
+          <TouchableWithoutFeedback>
+            <View style={styles.modalView}>
+              <MyText style={styles.title}>Filter</MyText>
+              <View style={styles.body}>
+                <MyText style={[TextStyles.bodyM, { color: colors.gray02 }]}>
+                  You can select multiple roles
+                </MyText>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  marginTop: 8,
+                  marginBottom: 16
+                }}
+              >
+                <ButtonListCheckbox
+                  heading="Select Roles"
+                  items={Array.from({ length: 7 }, (_, index) => ({
+                    value: `role-${index}`,
+                    label: `Moderator`
+                  }))}
+                  scrollable
+                  values={selectedRoles}
+                  onAdd={(value: string) =>
+                    setSelectedRoles([...selectedRoles, value])
+                  }
+                  onRemove={(value: string) =>
+                    setSelectedRoles(
+                      selectedRoles.filter((role) => role !== value)
+                    )
+                  }
+                />
+              </View>
+              <MyButtonText
+                title="Cancel"
+                reverseStyle
+                containerStyle={{
+                  marginTop: 'auto',
+                  width: '100%'
+                }}
+                onPress={props.onClose}
+              />
+              <MyButtonText
+                showOutline={false}
+                title="Show 4 results"
+                containerStyle={{
+                  marginTop: 10,
+                  width: '100%'
+                }}
+                onPress={props.onClose}
               />
             </View>
-            <MyButtonText
-              title="Cancel"
-              reverseStyle
-              containerStyle={{
-                marginTop: 'auto',
-                width: '100%'
-              }}
-              onPress={props.onClose}
-            />
-            <MyButtonText
-              showOutline={false}
-              title="Show 4 results"
-              containerStyle={{
-                marginTop: 10,
-                width: '100%'
-              }}
-              onPress={props.onClose}
-            />
-          </View>
+          </TouchableWithoutFeedback>
         </View>
       </TouchableWithoutFeedback>
     </Modal>
@@ -94,7 +96,7 @@ const FilterModal = (props: FilterModalProps) => {
 export default FilterModal;
 
 const styles = StyleSheet.create({
-  modelContainer: {
+  modalContainer: {
     ...GlobalStyles.container,
     flex: 1,
     justifyContent: 'center',
