548c81d0ee6fc95195b1a044c68e7cf3368e1c7b
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-07-30 16:26:25 +0700

    FE-29: move member and user ban item to folder userManagement

--- components/userManagment/MemberItem.tsx
+++ components/userManagment/MemberItem.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import MyText from './MyText';
+import MyText from '../MyText';
 import { colors, fonts } from '@/constants/theme';
 
 const MemberItem = () => {
