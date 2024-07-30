94b2537d2c09b899f2ecf64ed6500660c4c9e709
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-07-30 16:25:08 +0700

    FE-29: add user ban item

---
+++ components/UserBanItem.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MyText from './MyText';
+import { colors, fonts } from '@/constants/theme';
+
+const UserBanItem = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.avatar}></View>
+      <MyText style={styles.username}>username</MyText>
+    </View>
+  );
+};
+
+export default UserBanItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    columnGap: 16,
+    alignItems: 'center'
+  },
+  avatar: {
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+    backgroundColor: colors.gray03
+  },
+  username: {
+    fontSize: 14,
+    fontFamily: fonts.bold
+  }
+});
