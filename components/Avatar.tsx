ec8a1c682ae1ff19951f2d71a42370e752cd90b0
Truong Chi Nhan <nhanbin03@gmail.com>
2024-08-04 17:27:40 +0700

    FE-38: Integrate status text in user item

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -3,19 +3,18 @@ import { colors } from '@/constants/theme';
 import { DefaultProfileImage } from '@/constants/images';
 import { getOnlineStatusColor } from '@/utils/user';
 import { StatusType } from '@/types/user_status';
-import { useApolloClient } from '@apollo/client';
-import { useFocusEffect } from 'expo-router';
-import { useCallback, useState } from 'react';
-import { USER_STATUS_SUBSCRIPTION } from '@/services/graphql/subscriptions';
-import { getData } from '@/utils/api';
 
-export interface AvatarProps {
+export interface AvatarCommonProps {
   id: string;
   profilePic?: string;
   showStatus?: boolean;
   onlineStatus?: StatusType;
-  subscribeToStatus?: boolean;
   avatarStyle?: ImageStyle;
+}
+
+interface AvatarProps extends AvatarCommonProps {
+  isOnline?: boolean;
+  statusType?: StatusType;
 }
 
 const Avatar = ({
@@ -23,43 +22,11 @@ const Avatar = ({
   profilePic,
   showStatus,
   onlineStatus,
-  subscribeToStatus,
-  avatarStyle
+  avatarStyle,
+  isOnline,
+  statusType
 }: AvatarProps) => {
-  const wsClient = useApolloClient();
-  const [isOnline, setIsOnline] = useState(false);
-  const [statusType, setStatusType] = useState(StatusType.OFFLINE);
-
   const combinedStyles = StyleSheet.flatten([styles.profilePic, avatarStyle]);
-
-  useFocusEffect(
-    useCallback(() => {
-      if (!subscribeToStatus) return;
-
-      getData(`/api/v1/status/${id}`)
-        .then((res) => {
-          setIsOnline(res?.is_online);
-          setStatusType(res?.type);
-        })
-        .catch(() => {
-          setIsOnline(false);
-          setStatusType(StatusType.OFFLINE);
-        });
-
-      const observable = wsClient.subscribe({
-        query: USER_STATUS_SUBSCRIPTION,
-        variables: { user_id: id }
-      });
-      const cleanup = observable.subscribe({
-        next({ data: { userStatusChanged } }) {
-          setIsOnline(userStatusChanged?.is_online);
-          setStatusType(userStatusChanged?.type);
-        }
-      });
-      return () => cleanup.unsubscribe();
-    }, [subscribeToStatus])
-  );
-
   return (
     <View>
       <Image
