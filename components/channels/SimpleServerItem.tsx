cf0672a556abce347b533767e7d6793f290438b9
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-08-10 14:47:06 +0700

    FE-40: add simple server item

---
+++ components/channels/SimpleServerItem.tsx
@@ -0,0 +1,62 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { colors } from '@/constants/theme';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated';
+
+interface SimpleServerItemProps {
+  id: string;
+  selected?: boolean;
+  onPress?: (id: string) => void;
+}
+
+const SimpleServerItem = (props: SimpleServerItemProps) => {
+  const [flag, setFlag] = useState(false);
+
+  const isControlled = props.selected !== undefined;
+  const state = isControlled ? props.selected : flag;
+
+  const animatedStyle = useAnimatedStyle(
+    () => ({
+      borderWidth: withTiming(state ? 3 : 0, { duration: 300 }) // Adjust duration as needed
+    }),
+    [state]
+  );
+
+  const handlePress = () => {
+    if (!isControlled) {
+      setFlag(!flag);
+    }
+    props.onPress?.(props.id);
+  };
+
+  return (
+    <Pressable onPress={handlePress}>
+      <Animated.View style={[styles.container, animatedStyle]}>
+        <View style={styles.serverImg} />
+      </Animated.View>
+    </Pressable>
+  );
+};
+
+export default SimpleServerItem;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 52,
+    height: 52,
+    borderRadius: 26,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: colors.primary
+  },
+  serverImg: {
+    width: 44,
+    height: 44,
+    borderRadius: 44 / 2,
+    backgroundColor: colors.gray02
+  }
+});
