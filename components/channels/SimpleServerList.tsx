78ddae0439886b0430ad02d6d270590d71b0bb11
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-08-10 15:13:32 +0700

    FE-40: add simple server list

---
+++ components/channels/SimpleServerList.tsx
@@ -0,0 +1,38 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ServerListProps } from '@/types';
+import SimpleServerItem from './SimpleServerItem';
+
+interface SimpleServerListProps extends ServerListProps {
+  currentServerId: string;
+}
+
+const SimpleServerList = (props: SimpleServerListProps) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        horizontal
+        data={props.servers}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={{ marginLeft: 8 }}>
+            <SimpleServerItem
+              id={item.id}
+              selected={item.id === props.currentServerId}
+              onPress={props.onChange}
+            />
+          </View>
+        )}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+export default SimpleServerList;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 7
+  }
+});
--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,17 @@ export interface IconProps {
   color?: string;
 }
 
+export interface Server {
+  id: string;
+  name: string;
+}
+
 export interface ServerItemProps {
   id: string;
   onPress?: (id: string) => void;
+}
+
+export interface ServerListProps {
+  servers: Server[];
+  onChange: (id: string) => void;
 }
