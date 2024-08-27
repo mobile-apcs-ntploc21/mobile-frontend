576d6d297477bde241ea88990ca9e487de154fd2
Truong Chi Nhan <nhanbin03@gmail.com>
2024-08-27 10:49:12 +0700

    FE-50: Add ButtonListToggle component for toggling items

---
+++ components/ButtonList/ButtonListToggle.tsx
@@ -0,0 +1,78 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import ButtonListBase, { ButtonListBaseProps } from './ButtonListBase';
+import MyText from '../MyText';
+import { TextStyles } from '@/styles/TextStyles';
+import IconWithSize from '../IconWithSize';
+import TickIcon from '@/assets/icons/TickIcon';
+import { colors } from '@/constants/theme';
+import ToggleSwitch from 'toggle-switch-react-native';
+
+interface ButtonListToggleItem {
+  value: string;
+  label?: string;
+  isOn?: boolean;
+  onChange?: (isOn: boolean) => void;
+}
+
+interface ButtonListToggleProps extends Omit<ButtonListBaseProps, 'items'> {
+  items?: ButtonListToggleItem[];
+}
+
+const ToggleItem = ({
+  item,
+  index
+}: {
+  item: ButtonListToggleItem;
+  index: number;
+}) => {
+  const [isOn, setIsOn] = useState(item.isOn ?? false);
+  return (
+    <View style={styles.radioContainer} key={index}>
+      <MyText style={styles.label}>{item.label}</MyText>
+      <ToggleSwitch
+        isOn={item.isOn ?? isOn}
+        onColor={colors.primary}
+        offColor={colors.gray03}
+        size="medium"
+        onToggle={(isOn: boolean) => {
+          setIsOn(isOn);
+          item.onChange?.(isOn);
+        }}
+      />
+    </View>
+  );
+};
+
+const ButtonListToggle = (props: ButtonListToggleProps) => {
+  return (
+    <ButtonListBase
+      {...props}
+      disabled
+      items={props.items?.map((item, index) => ({
+        itemComponent: <ToggleItem item={item} index={index} />
+      }))}
+    />
+  );
+};
+
+export default ButtonListToggle;
+
+const styles = StyleSheet.create({
+  radioContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  radio: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: '#000'
+  },
+  label: {
+    ...TextStyles.bodyXL,
+    marginLeft: 4
+  }
+});
