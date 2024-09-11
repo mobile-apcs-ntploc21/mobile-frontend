0df021e8c9e86c780d045cc1c89fec2427b7200d
Truong Chi Nhan <nhanbin03@gmail.com>
2024-09-11 22:42:00 +0700

    FE-73: Implement mentions to ServerChatInput

--- components/Chat/ServerChatInput.tsx
+++ components/Chat/ServerChatInput.tsx
@@ -1,12 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useMemo } from 'react';
 import BaseChatInput, { BaseChatInputProps } from './BaseChatInput';
+import useServer from '@/hooks/useServer';
 
 interface ServerChatInputProps
   extends Omit<BaseChatInputProps, 'mentions' | 'emojis' | 'channels'> {}
 
 const ServerChatInput = (props: ServerChatInputProps) => {
-  return <BaseChatInput {...props} />;
+  const { emojis, members, roles, categories } = useServer();
+  const channels = useMemo(() => {
+    return categories.map((category) => category.channels).flat();
+  }, [categories]);
+  return (
+    <BaseChatInput
+      {...props}
+      // mentions both members and roles
+      mentions={[
+        ...members.map((member) => member.username),
+        ...roles.map((role) => (role.default ? 'everyone' : role.name))
+      ]}
+      emojis={emojis.map((emoji) => emoji.name)}
+      channels={channels.map((channel) => channel.name)}
+    />
+  );
 };
 
 export default ServerChatInput;
