4ff8da0d0c54e11195b106a11e247f36646cf471
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-09-12 18:46:23 +0700

    FE-76: Add handle subscription data and support for multiple subscriptions
    This commit adds the ability to handle subscription data and support for multiple subscriptions. The handle subscription data function is called when the subscription data is received from the server, with given support multiple servers subscription, the data now can be handled properly across multiple servers.

--- context/ServerProvider.tsx
+++ context/ServerProvider.tsx
@@ -1,4 +1,7 @@
-import { SERVER_SUBSCRIPTION } from '@/services/graphql/subscriptions';
+import {
+  SERVERS_SUBSCRIPTION,
+  SERVER_SUBSCRIPTION
+} from '@/services/graphql/subscriptions';
 import { ServerEvents, ServerProfile } from '@/types';
 import { Category, Emoji, Role } from '@/types/server';
 import { getData } from '@/utils/api';
@@ -179,11 +182,242 @@ export const ServerProvider = (props: ProviderProps) => {
   const [servers, setServers] = useState<Record<string, ServerState>>({});
   const [state, dispatch] = useReducer(reducer, initialState);
   const { dispatch: conversationDispatch } = useConversations();
-  const fetchedServerIds = Object.keys(servers);
-  const { data: subscriptionData } = useSubscription(SERVER_SUBSCRIPTION, {
-    variables: { server_id: state.server_id },
-    skip: !state.server_id
+  const fetchedServerIds = Object.keys(servers) || [];
+  const { data: subscriptionData } = useSubscription(SERVERS_SUBSCRIPTION, {
+    variables: { server_ids: fetchedServerIds, user_id: null },
+    skip: fetchedServerIds.length === 0
   });
+
+  const handleSubscriptionData = async (subscriptionData: any) => {
+    console.log('Subscription data:', subscriptionData);
+    if (!subscriptionData) return;
+
+    const { serversUpdated: serverData } = subscriptionData;
+    const { type, server_id, data } = serverData;
+
+    if (!serverData) return;
+
+    // Handle the subscription data based on the event type
+    // Please assign the type and payload to the following variables
+    let dispatchLoad: {
+      type: ServerActions | null;
+      payload: any;
+    }[] = [];
+
+    // Please add the missing cases
+    switch (type) {
+      case ServerEvents.userStatusChanged:
+        dispatchLoad.push({
+          type: ServerActions.UPDATE_STATUS,
+          payload: data
+        });
+        break;
+      case ServerEvents.userProfileChanged:
+        dispatchLoad.push({
+          type: ServerActions.UPDATE_PROFILE,
+          payload: data
+        });
+        break;
+      case ServerEvents.memberJoined:
+        (async () => {
+          let profileAndStatus = await getServerProfile(data, true).catch(() =>
+            getServerProfile(data)
+          );
+
+          dispatchLoad.push({
+            type: ServerActions.SET_MEMBERS,
+            payload: [...state.members, profileAndStatus]
+          });
+        })();
+        break;
+      case ServerEvents.memberLeft:
+        dispatchLoad.push({
+          type: ServerActions.SET_MEMBERS,
+          payload: state.members.filter(
+            (member) => member.user_id !== data.user_id
+          )
+        });
+        break;
+      case ServerEvents.memberAdded:
+        (async () => {
+          const profileAndStatus = await Promise.all(
+            // @ts-ignore
+            serverUpdated.data.map((user_id) =>
+              getServerProfile(user_id, true).catch(() =>
+                getServerProfile(user_id)
+              )
+            )
+          );
+          dispatchLoad.push({
+            type: ServerActions.SET_MEMBERS,
+            payload: [...state.members, ...profileAndStatus]
+          });
+        })();
+        break;
+      case ServerEvents.memberRemoved:
+        dispatchLoad.push({
+          type: ServerActions.SET_MEMBERS,
+          payload: state.members.filter(
+            (member) => !data.includes(member.user_id)
+          )
+        });
+        break;
+      case ServerEvents.userRoleAdded:
+        {
+          const members = [...state.members];
+          members.forEach(
+            (member) =>
+              member.user_id === data.user_id &&
+              member.roles.push(
+                state.customRoles.find((role) => role.id === data.role_id)!
+              )
+          );
+          // Set the members with the updated roles
+          dispatchLoad.push({
+            type: ServerActions.SET_MEMBERS,
+            payload: members
+          });
+          // Increment the role count
+          dispatchLoad.push({
+            type: ServerActions.SET_ROLES,
+            payload: servers[server_id].roles.map((role) =>
+              role.id === data.role_id
+                ? { ...role, number_of_users: role.number_of_users + 1 }
+                : role
+            )
+          });
+        }
+        break;
+      case ServerEvents.userRoleDeleted:
+        {
+          const members = [...state.members];
+          members.forEach(
+            (member) =>
+              member.user_id === data.user_id &&
+              member.roles.filter((role) => role.id !== data.role_id)
+          );
+          // Set the members with the updated roles
+          dispatchLoad.push({
+            type: ServerActions.SET_MEMBERS,
+            payload: members
+          });
+          // Decrement the role count
+          dispatchLoad.push({
+            type: ServerActions.SET_ROLES,
+            payload: servers[server_id].roles.map((role) =>
+              role.id === data.role_id
+                ? { ...role, number_of_users: role.number_of_users - 1 }
+                : role
+            )
+          });
+        }
+        break;
+      case ServerEvents.emojiAdded:
+        dispatchLoad.push({
+          type: ServerActions.SET_EMOJI,
+          payload: [
+            ...servers[server_id].emojis,
+            {
+              id: data._id,
+              ...data
+            }
+          ]
+        });
+        break;
+      case ServerEvents.emojiUpdated:
+        dispatchLoad.push({
+          type: ServerActions.SET_EMOJI,
+          payload: servers[server_id].emojis.map((emoji) =>
+            emoji.id === data._id ? { ...emoji, ...data } : emoji
+          )
+        });
+
+        break;
+      case ServerEvents.emojiDeleted:
+        dispatchLoad.push({
+          type: ServerActions.SET_EMOJI,
+          payload: servers[server_id].emojis.filter(
+            (emoji) => emoji.id !== data._id
+          )
+        });
+        break;
+      case ServerEvents.roleAdded:
+        dispatchLoad.push({
+          type: ServerActions.SET_ROLES,
+          payload: [
+            ...servers[server_id].roles,
+            {
+              id: data._id,
+              number_of_users: 0,
+              ...data
+            }
+          ]
+        });
+        break;
+      case ServerEvents.roleUpdated:
+        dispatchLoad.push({
+          type: ServerActions.SET_ROLES,
+          payload: servers[server_id].roles.map((role) =>
+            role.id === data._id ? { ...role, ...data } : role
+          )
+        });
+        break;
+      case ServerEvents.roleDeleted:
+        dispatchLoad.push({
+          type: ServerActions.SET_ROLES,
+          payload: servers[server_id].roles.filter(
+            (role) => role.id !== data._id
+          )
+        });
+
+        // Remove the role from the members
+        const members = [...servers[server_id].members];
+        members.forEach((member) => {
+          member.roles = member.roles.filter((role) => role.id !== data._id);
+        });
+        dispatchLoad.push({
+          type: ServerActions.SET_MEMBERS,
+          payload: members
+        });
+        break;
+      case ServerEvents.channelAdded:
+        dispatchLoad.push({
+          type: ServerActions.CREATE_CHANNEL,
+          payload: data
+        });
+        break;
+      default:
+        console.warn('Unknown event type:', type);
+        console.log(type, server_id, data);
+    }
+
+    // Dispatch the action if the server is active
+    if (activeServerIdRef.current === server_id) {
+      dispatchLoad.forEach((load) => {
+        dispatch({
+          type: load.type || ServerActions.INIT,
+          payload: load.payload
+        });
+      });
+    } else {
+      // Else update the other servers data using the reducer
+      dispatchLoad.forEach((load) => {
+        setServers((prevServers) => ({
+          ...prevServers,
+          [server_id]: reducer(prevServers[server_id], {
+            type: load.type || ServerActions.INIT,
+            payload: load.payload
+          })
+        }));
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (subscriptionData) {
+      handleSubscriptionData(subscriptionData);
+    }
+  }, [subscriptionData, dispatch]);
 
   const getServerProfile = useCallback(
     async (id: string, fromServer?: boolean) => {
@@ -206,200 +440,6 @@ export const ServerProvider = (props: ProviderProps) => {
     },
     [state.server_id]
   );
-
-  useEffect(() => {
-    if (!subscriptionData) return;
-    const { serverUpdated } = subscriptionData;
-    console.log(serverUpdated);
-    switch (serverUpdated.type) {
-      case ServerEvents.userStatusChanged:
-        dispatch({
-          type: ServerActions.UPDATE_STATUS,
-          payload: serverUpdated.data
-        });
-        break;
-      case ServerEvents.userProfileChanged:
-        dispatch({
-          type: ServerActions.UPDATE_PROFILE,
-          payload: serverUpdated.data
-        });
-        break;
-      case ServerEvents.memberJoined:
-        (async () => {
-          let profileAndStatus = await getServerProfile(
-            serverUpdated.data,
-            true
-          ).catch(() => getServerProfile(serverUpdated.data));
-          dispatch({
-            type: ServerActions.SET_MEMBERS,
-            payload: [...state.members, profileAndStatus]
-          });
-        })();
-        break;
-      case ServerEvents.memberLeft:
-        dispatch({
-          type: ServerActions.SET_MEMBERS,
-          payload: state.members.filter(
-            (member) => member.user_id !== serverUpdated.data.user_id
-          )
-        });
-        break;
-      case ServerEvents.memberAdded:
-        (async () => {
-          const profileAndStatus = await Promise.all(
-            // @ts-ignore
-            serverUpdated.data.map((user_id) =>
-              getServerProfile(user_id, true).catch(() =>
-                getServerProfile(user_id)
-              )
-            )
-          );
-          dispatch({
-            type: ServerActions.SET_MEMBERS,
-            payload: [...state.members, ...profileAndStatus]
-          });
-        })();
-        break;
-      case ServerEvents.memberRemoved:
-        // @ts-ignore
-        dispatch({
-          type: ServerActions.SET_MEMBERS,
-          payload: state.members.filter(
-            (member) => !serverUpdated.data.includes(member.user_id)
-          )
-        });
-        break;
-      case ServerEvents.userRoleAdded:
-        {
-          const members = [...state.members];
-          members.forEach(
-            (member) =>
-              member.user_id === serverUpdated.data.user_id &&
-              member.roles.push(
-                state.customRoles.find(
-                  (role) => role.id === serverUpdated.data.role_id
-                )!
-              )
-          );
-          dispatch({
-            type: ServerActions.SET_MEMBERS,
-            payload: members
-          });
-          dispatch({
-            type: ServerActions.SET_ROLES,
-            payload: state.roles.map((role) =>
-              role.id === serverUpdated.data.role_id
-                ? {
-                    ...role,
-                    number_of_users: role.number_of_users + 1
-                  }
-                : role
-            )
-          });
-        }
-        break;
-      case ServerEvents.userRoleDeleted:
-        {
-          const members = [...state.members];
-          members.forEach((member) => {
-            if (member.user_id === serverUpdated.data.user_id)
-              member.roles = member.roles.filter(
-                (role) => role.id !== serverUpdated.data.role_id
-              );
-          });
-          dispatch({
-            type: ServerActions.SET_MEMBERS,
-            payload: members
-          });
-          dispatch({
-            type: ServerActions.SET_ROLES,
-            payload: state.roles.map((role) =>
-              role.id === serverUpdated.data.role_id
-                ? {
-                    ...role,
-                    number_of_users: role.number_of_users - 1
-                  }
-                : role
-            )
-          });
-        }
-        break;
-      case ServerEvents.emojiAdded:
-        dispatch({
-          type: ServerActions.SET_EMOJI,
-          payload: [
-            ...state.emojis,
-            {
-              id: serverUpdated.data._id,
-              ...serverUpdated.data
-            }
-          ]
-        });
-        break;
-      case ServerEvents.emojiUpdated:
-        dispatch({
-          type: ServerActions.SET_EMOJI,
-          payload: state.emojis.map((emoji) =>
-            emoji.id === serverUpdated.data._id
-              ? { ...emoji, ...serverUpdated.data }
-              : emoji
-          )
-        });
-        break;
-      case ServerEvents.emojiDeleted:
-        dispatch({
-          type: ServerActions.SET_EMOJI,
-          payload: state.emojis.filter(
-            (emoji) => emoji.id !== serverUpdated.data._id
-          )
-        });
-        break;
-      case ServerEvents.roleAdded:
-        dispatch({
-          type: ServerActions.SET_ROLES,
-          payload: [
-            ...state.roles,
-            {
-              id: serverUpdated.data._id,
-              number_of_users: 0,
-              ...serverUpdated.data
-            }
-          ]
-        });
-        break;
-      case ServerEvents.roleDeleted:
-        dispatch({
-          type: ServerActions.SET_ROLES,
-          payload: state.roles.filter(
-            (role) => role.id !== serverUpdated.data._id
-          )
-        });
-        const members = [...state.members];
-        members.forEach((member) => {
-          member.roles = member.roles.filter(
-            (role) => role.id !== serverUpdated.data._id
-          );
-        });
-        dispatch({
-          type: ServerActions.SET_MEMBERS,
-          payload: members
-        });
-        break;
-      case ServerEvents.roleUpdated:
-        dispatch({
-          type: ServerActions.SET_ROLES,
-          payload: state.roles.map((role) =>
-            role.id === serverUpdated.data._id
-              ? { ...role, ...serverUpdated.data }
-              : role
-          )
-        });
-        break;
-      default:
-        console.warn('Unknown event type:', serverUpdated.type);
-        console.log(serverUpdated);
-    }
-  }, [subscriptionData, dispatch]);
 
   // Fetch server data
   const fetchServerData = async (server_id: string) => {
